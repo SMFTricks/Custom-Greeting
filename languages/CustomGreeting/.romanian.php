211b2893387f32698e10e0d03cb5959c221545f0
danuthintariu <98109065+danuthintariu@users.noreply.github.com>
2023-04-21 22:24:13 +0100

    Update .romanian.php

--- languages/CustomGreeting/.romanian.php
+++ languages/CustomGreeting/.romanian.php
@@ -16,7 +16,7 @@ $txt['cgdt_title'] = 'Salut personalizat';
 $txt['cgdt_enable'] = 'Activează salutul personalizat <span class="smalltext">Aceasta va afișa diferite salutări în antetul forumului, în funcție de ora din zi. <br /> Mesajele sunt setate mai jos.</span>';
 $txt['cgdt_message1'] = 'CGDT Mesaj 1 <span class="smalltext">Afișat de la 01:00am până la 4:59am</span>';
 $txt['cgdt_message2'] = 'CGDT Mesaj 2 <span class="smalltext">Afișat de la 5:00am până la 7:59am</span>';
-$txt['cgdt_message3'] = 'CGDT Mesaj 3 <span class="smalltext">Afișat de la 8:00 până la 11:59am</span>';
+$txt['cgdt_message3'] = 'CGDT Mesaj 3 <span class="smalltext">Afișat de la 8:00am până la 11:59am</span>';
 $txt['cgdt_message4'] = 'CGDT Mesaj 4 <span class="smalltext">Afișat de la 12:00pm până la 6:59pm</span>';
 $txt['cgdt_message5'] = 'CGDT Mesaj 5 <span class="smalltext">Afișat de la 6:00pm până la 8:59pm</span>';
 $txt['cgdt_message6'] = 'CGDT Mesaj 6 <span class="smalltext">Afișat de la 9:00pm până la 12:59pm</span>';
